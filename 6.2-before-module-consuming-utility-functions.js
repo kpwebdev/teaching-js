129f1d383fc90c8b7d1d97ce2ee757ab03decd67
KP Web Dev <kpwebdevishere@gmail.com>
2024-06-10 11:50:25 +0530

    created '6.2-before-module-consuming-utility-functions.js' file and linked the same in index.html

---
+++ 6.2-before-module-consuming-utility-functions.js
@@ -0,0 +1,7 @@
+const x = 10;
+const y = 20;
+
+console.log("sum(x, y):", sum(x, y));
+console.log("sub(x, y):", sub(x, y));
+console.log("mul(x, y):", mul(x, y));
+console.log("div(x, y):", div(x, y));
