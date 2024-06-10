20dc5462d6f43ed3d132be90fb794ac15497f47f
KP Web Dev <kpwebdevishere@gmail.com>
2024-06-10 13:06:18 +0530

    created '6.4-after-module-consuming-utility-functions.js' script file and linked the same in index.html

---
+++ 6.4-after-module-consuming-utility-functions.js
@@ -0,0 +1,34 @@
+// name import
+import { sum2, sub2, mul2, div2 } from "./6.3-after-module-utility-functions";
+// import alias
+import {
+  sum2 as sumFunc,
+  sub2 as subFunc,
+  mul2 as mulFunc,
+  div2 as divFunc,
+} from "./6.3-after-module-utility-functions";
+// import all name exports and add them into a single variable
+import * as mathFunctions2 from "./6.3-after-module-utility-functions";
+
+// default import
+import mathFunctions from "./6.3-after-module-utility-functions";
+
+console.log("sum2(x, y):", sum2(x, y));
+console.log("sub2(x, y):", sub2(x, y));
+console.log("mul2(x, y):", mul2(x, y));
+console.log("div2(x, y):", div2(x, y));
+
+console.log("mathFunctions.sum3(x, y):", mathFunctions.sum3(x, y));
+console.log("mathFunctions.sub3(x, y):", mathFunctions.sub3(x, y));
+console.log("mathFunctions.mul3(x, y):", mathFunctions.mul3(x, y));
+console.log("mathFunctions.div3(x, y):", mathFunctions.div3(x, y));
+
+console.log("mathFunctions2.sum2(x, y):", mathFunctions2.sum2(x, y));
+console.log("mathFunctions2.sub2(x, y):", mathFunctions2.sub2(x, y));
+console.log("mathFunctions2.mul2(x, y):", mathFunctions2.mul2(x, y));
+console.log("mathFunctions2.div2(x, y):", mathFunctions2.div2(x, y));
+
+console.log("sumFunc(x, y):", sumFunc(x, y));
+console.log("subFunc(x, y):", subFunc(x, y));
+console.log("mulFunc(x, y):", mulFunc(x, y));
+console.log("divFunc(x, y):", divFunc(x, y));
