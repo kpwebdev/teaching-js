39f144957b5b4937b0c6405f6126b9dde6f93d99
KP Web Dev <kpwebdevishere@gmail.com>
2024-07-08 16:21:39 +0530

    add close useState closure example in 8.closures.js

--- 8.closures.js
+++ 8.closures.js
@@ -10,3 +10,34 @@ button.addEventListener(
     };
   })()
 );
+
+console.clear();
+
+function useState(initialState) {
+  let state = initialState || { userName: "KP" };
+
+  function setState(updatedState) {
+    // if(typeof state !== "string") throw new TypeError("invalid type detected, you can only provide string.");
+    if (state === updatedState) {
+      console.log("values are the same");
+      return;
+    }
+
+    state = updatedState;
+  }
+
+  function getState() {
+    return state;
+  }
+
+  return [getState, setState];
+}
+
+const [getUser, setUser] = useState({ userName: "Sai Prasad" } /*0x123456*/);
+const user = getUser(); /*0x123456*/
+console.log(user);
+user.userName = "Radhika"; /*0x123456*/ /*memory location is still the same*/
+const updatedUser = { ...user, userName: "Randhika" };
+setUser(updatedUser);
+console.log(getUser());
+console.log("user === updatedUser", user === updatedUser);
