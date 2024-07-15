8d294fcc79c19a00833eebb9a3c9045b97ba0cd7
KP Web Dev <kpwebdevishere@gmail.com>
2024-07-15 17:50:40 +0530

    cover DOM manupulation methods part 1

--- 9.DOM-manupulation.js
+++ 9.DOM-manupulation.js
@@ -46,7 +46,7 @@ function createNestedUlAndAppend() {
   liWithIdSelectedByQuery.appendChild(nestedUl);
 }
 
-setTimeout(createNestedUlAndAppend, 5000);
+// setTimeout(createNestedUlAndAppend, 5000);
 
 // it will give you all the element that were present when you call this method but whatever you add in the future will not be there
 // if you want access whatever comes in the future use the children prop available on the parent
@@ -55,3 +55,101 @@ console.log(allLiSelectedByQueryAll);
 
 const ul = document.querySelector("ul");
 const constantlyUpdatedUlItems = ul.children;
+
+// document fragment
+function create20LiAfterDelay(delayInMs) {
+  const liItemsFragments = document.createDocumentFragment();
+
+  for (let i = 1; i <= 20; i++) {
+    setTimeout(() => {
+      const newLi = createLi(`Item from Loop ${i}`);
+      liItemsFragments.appendChild(newLi);
+
+      if (i >= 20) ul.appendChild(liItemsFragments);
+    }, i * delayInMs);
+  }
+}
+
+function createLi(liText) {
+  const liTextNode = document.createTextNode(liText);
+  const newLi = document.createElement("li");
+
+  newLi.appendChild(liTextNode);
+
+  return newLi;
+}
+
+// create20LiAfterDelay(1000);
+
+// innerHTML
+function showAndChangeInnerHTML() {
+  const firstLi = ul.children[0];
+  console.log("firstLi.innerHTML", firstLi.innerHTML);
+  firstLi.innerHTML = "Item 1 is changed Now";
+  console.log("firstLi.innerHTML (after change)", firstLi.innerHTML);
+}
+// innerText
+function showAndChangeInnerText() {
+  const secondLi = ul.children[1];
+  console.log("secondLi.innerText", secondLi.innerText);
+  secondLi.innerText = "Item 2 is changed Now";
+  console.log("secondLi.innerText (after change)", secondLi.innerText);
+}
+// textContent
+function showAndChangeTextContent() {
+  const thirdLi = ul.children[2];
+  console.log("thirdLi.textContent", thirdLi.textContent);
+  thirdLi.textContent = "Item 3 is changed Now";
+  console.log("thirdLi.textContent (after change)", thirdLi.textContent);
+}
+
+// showAndChangeInnerHTML();
+// showAndChangeInnerText();
+// showAndChangeTextContent();
+
+function showUlWithInnerHTML() {
+  console.log("ul.innerHTML", ul.innerHTML);
+}
+function showUlWithOuterHTML() {
+  console.log("ul.outerHTML", ul.outerHTML);
+}
+function showUlWithInnerText() {
+  console.log("ul.innerText", ul.innerText);
+}
+function showUlWithTextContent() {
+  console.log("ul.textContent", ul.textContent);
+}
+
+// showUlWithInnerHTML();
+// showUlWithOuterHTML();
+// showUlWithInnerText();
+// showUlWithTextContent();
+
+// with append you can add many multiple types of nodes at once
+function createAndAppendOnUl() {
+  const newLi = document.createElement("li");
+  newLi.textContent = "I am new";
+  const textNodeOnRoot = document.createTextNode(
+    "I am a text node on the root level of UL"
+  );
+
+  ul.append(textNodeOnRoot, "Text Before the newLi", newLi);
+}
+
+// createAndAppendOnUl();
+
+// remove
+function removeFirstLi() {
+  const firstLi = ul.children[0];
+
+  firstLi.remove();
+}
+
+// removeFirstLi();
+
+// removeChild
+// const ul2 = document
+function removeChildFromUl() {
+  const secondChild = ul.children[1];
+  ul.removeChild(secondChild);
+}
